2ff6f0010864fb349bfbcaa14cb49b48903a0cdf
voidgogo <2021038@daelim.ac.kr>
2025-09-25 18:21:39 +0900

    week07 v1.4 virtual inheritance

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class Person {
+class Person {  
 public:
 	string name;
-	virtual void warn() = 0;
+	//virtual void warn() = 0;
 };
-class UndergraduateStudent : public Person{
+class UndergraduateStudent : virtual public Person{
 public:
 	double gpa;
 	void warn() { cout << "학사경고\n"; };
 };
-class DormitoryStudent : public Person {
+class DormitoryStudent : virtual public Person {
 public:
 	int roomNumber;
 	void warn() { cout << "벌점부여\n"; };
@@ -25,6 +25,8 @@ int main() {
 	uds.DormitoryStudent::warn();
 	uds.gpa = 3.9;
 	uds.roomNumber = 201;
-	//uds.name = "Kim";  // Error. 죽음의 다이아몬드 문제
+	uds.name = "Kim";  // 양쪽 부모클래스가 할아버지 클래스를 상속받을 때 virtual 키워드를 이용해서 죽음의 다이아몬드 문제를 해결
+
+	cout << uds.name << "의 평점은 " << uds.gpa << "이고, 기숙사 방번호는 " << uds.roomNumber << "호 입니다\n";
 	return 0;
 }
