7dfaa3e2d3d9eb8abe09cf22a3f3ce0187175ed2
voidgogo <2021038@daelim.ac.kr>
2025-09-25 11:50:11 +0900

    week05 v1.1 abstract class

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -2,9 +2,10 @@
 #include <string>
 #include <typeinfo> 
 using namespace std;
-class Animal {
+class Animal {  // abstract class
 public:
-	virtual void makeSound() { cout << "동물이 소리를 냅니다...\n"; }
+	//virtual void makeSound() { cout << "동물이 소리를 냅니다...\n"; }
+	virtual void makeSound() = 0;  // pure virtual function
 };
 class Dog : public Animal {
 public:
@@ -16,24 +17,10 @@ public:
 };
 int main()
 {
-	Animal* pa = new Dog();
-	pa->makeSound();
-	cout << pa << endl;
+	//Animal animal;
+	//animal.makeSound();
 
-	//Cat* pc = (Cat*)pa;  // Dangerous behavior!!!
-	//Cat* pc = dynamic_cast<Cat*>(pa);  // SAFE. 고양이클래스의 포인터가 강아지클래스의 메모리 번지주소를 받을 수 없다 (NULL 값 리턴)
-	Dog* pc = dynamic_cast<Dog*>(pa);  // 힙에 할당된 강아지클래스 객체의 메모리 번지 주소를 받는다
-	cout << pc << endl;
-	pc->makeSound();	
-	delete pc;
-	pc = nullptr;
-
-	//Dog* pd = (Dog*)pa;  // downcasting, old style (C style)
-	//Dog* pd = dynamic_cast<Dog*>(pa);  // downcasting, modern style (C++11)
-	//pd->makeSound();
-	//delete pd;
-	//pd = nullptr;
+	Animal animal;  // error
 
 	return 0;
 }
-// git reset --hard HEAD~삭제할커밋개수
