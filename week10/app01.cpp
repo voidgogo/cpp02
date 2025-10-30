f0f989917a06e91b6b489df4514f02b33f0e50ce
voidgogo <2021038@daelim.ac.kr>
2025-10-30 11:33:25 +0900

    week10 v2.3 operator overloading (type conversion)

---
+++ week10/app01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	//double c = 3 * 7 * 2.2;  // 묵시적 형변환
+	int c = 3 * 7 * 2.2;  // 묵시적 형변환
+
+	cout << c << endl;
+	return 0;
+}
