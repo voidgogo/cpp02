a50e876f0bdd9e6123a6006e0fa729a84303453c
voidgogo <2021038@daelim.ac.kr>
2025-11-27 11:05:51 +0900

    week13 v3.9 class template

---
+++ week14/app01.cpp
@@ -0,0 +1,46 @@
+﻿#include "stack.cpp"
+
+class Pokemon {
+private:
+	int hp;
+	string name;
+public:
+	Pokemon() : hp(1), name("무명") {
+
+	}
+	Pokemon(int hp, string name) : hp(hp), name(name) {
+		cout << name << " 포켓몬 생성됨\n";
+	}
+};
+//class Pikachu : public Pokemon {
+//public:
+//	Pikachu() {};
+//	void attack() { "전기공격!\n"; };
+//};
+//class Squirtle : public Pokemon {
+//public:
+//	Squirtle() {};
+//	void attack() { "물대포공격!\n"; };
+//};
+
+int main()
+{
+	Pokemon pikachu;
+	Pokemon squirtle(100, "꼬부기");
+
+  Stack<int> stacki(10);
+  Stack<Pokemon> stackPokemon(3);
+  Stack<double> stackd(3);
+  stackPokemon.push(pikachu);
+  stackPokemon.push(squirtle);
+
+  stacki.push(5);
+  stacki.push(6);
+  stacki.push(7);
+  stacki.push(3);
+  cout << stacki.pop() << endl;
+  cout << stacki.pop() << endl;
+  cout << stackd.pop() << endl;  // stack is empty!
+  
+  return 0;
+}
---
+++ week14/stack.h
@@ -0,0 +1,20 @@
+﻿#ifndef STACK_H
+#define STACK_H
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template <typename T>
+class Stack
+{
+  private:
+    T* ptr;
+    int capacity;
+    int size;
+  public:
+    Stack(int capacity);
+    ~Stack();
+    void push(const T& element);
+    T pop();  
+};
+#endif
